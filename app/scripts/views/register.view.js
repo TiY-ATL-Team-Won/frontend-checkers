13b399d6a441243715235e571761bdd87c4084f2
John Albritton <jtareb1@att.net>
2015-02-18 16:56:25 -0500

    wed afternoon commit

--- app/scripts/views/register.view.js
+++ app/scripts/views/register.view.js
@@ -25,7 +25,8 @@ app.RegisterView = Backbone.View.extend ({
   		e.preventDefault();
 
   		 var email = $('input[name="email"]').val();
-    	 var pw1    = $('input[name="password1"]').val();
+  
+       var pw1    = $('input[name="password1"]').val();
        var pw2    = $('input[name="password2"]').val();
 
     		if (email.indexOf('@') === -1) {
@@ -33,20 +34,21 @@ app.RegisterView = Backbone.View.extend ({
       			return;
     		}
 
-    		if (pw.length < 8) {
-      			$('span').html("Password is too short");
-      			return;
-    		}
 
     	 	if (pw1 !== pw2) {
     	 		$('span').html("Passwords do not match");
     	 		return;
     	 	}
 
+        if (pw1.length < 8) {
+            $('span').html("Password is too short");
+            return;
+        }
+
         var payload = {
             user: {
             email: email,
-        password: pw
+            password: pw1
       }
     };
 
@@ -62,15 +64,15 @@ app.RegisterView = Backbone.View.extend ({
      app.user = new app.User({ auth_token: data.user.authentication_token,
                                 email: data.user.email
                               }); 
-
+     app.user.navigate = ('user', {trigger: true});
      
-    })
+    
     .fail( function() {
         $('span').html("ERROR!!! " + JSON.stringify(payload));
       });
 
 
-   }, 
+   } 
     		   
   
 

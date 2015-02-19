18b9a1c4e83d3f090d92544e949b01438d1a7c9b
John Albritton <jtareb1@att.net>
2015-02-18 21:48:33 -0500

    added rank.view and rank.handlebars

--- app/scripts/views/rank.view.js
+++ app/scripts/views/rank.view.js
@@ -2,30 +2,33 @@
 
 'use strict'
 
-
-app.RankView = Backbone.View.extend ({
+app.RankView = Backbone.View.extend({
+	className: 'rank container',
 
 	events: {
-		//unknown at this time//
+		//unknown right now//
 
-	}.
-
-template: Handlebars.templates.rank,
-
-
-initialize: function() {
-		this.render();
 	},
 
-render: function() {
-  $('.display').html(this.el);
-   this.$el.html(this.template());
-  },
+	template: Handlebars.templates.rank,
 
+	initialize: function() {
+    this.render();
+  },
+  	render: function() {
+    $('.display').html(this.el);
+    this.$el.html(this.template(app.user.attributes));
+  }
+
+  	
+    
 
 });
 
+
 }());
+
+
 
 
 

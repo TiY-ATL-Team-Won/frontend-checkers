14e39859994ba58c0285ee663773f8fa33ef56ac
John Albritton <jtareb1@att.net>
2015-02-22 18:47:29 -0500

    fix errors

--- app/scripts/views/rank.view.js
+++ app/scripts/views/rank.view.js
@@ -6,58 +6,30 @@ app.RankView = Backbone.View.extend({
 	className: 'rank-container',
 
 	events: {
-<<<<<<< HEAD
+
 		'submit': 'leaderboard',
-=======
-		'submit': 'Leaderboard',
->>>>>>> staging
-    	//unknown right now//
 	},
 
 	template: Handlebars.templates.rank,
 
 	initialize: function() {
     	this.render();
-<<<<<<< HEAD
-    	app.players = app.players || [];
-    	//console.log('this');
-=======
+
+    
+
     	
->>>>>>> staging
+
   	},
   	
-	render: function() {
-<<<<<<< HEAD
-  		$.get(app.rootUrl + 'users/leaderboard/').done (function (data) {
-        
+	render: function(){
 
-       //var ranking = app.RankView; 
-       //console.log(ranking);
-	     //_.sortBy(ranking, 'experience');
-       //need to revers highest num first
-       //$('.display').html(this.el);
-      // this.$el.html(this.template());
-        
-      });
-
-    }, 
-
-          
-       
-  	
-        				
-
-=======
   		var self = this;
   		$.get(app.rootUrl + 'users/leaderboard/').done (function (data) {
-  		
-  		//var ranking =  new app.RankView; 
-  		//_.sortby(data, 'experience');
   		self.$el.html(this.template(data));
- 		// }
+ 		
   		});
 	},
->>>>>>> staging
+
 
 
  
